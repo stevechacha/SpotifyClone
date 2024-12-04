9747c815097d81cd0d287dd3699a66a3b4dcb6cb
Stephen  Chacha <stevechacha4@gmail.com>
2024-12-04 13:52:01 +0300

    Initial Commit

---
+++ SpotifyCloneTests/SpotifyCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneTests.swift
+//  SpotifyCloneTests
+//
+//  Created by stephen chacha on 04/12/2024.
+//
+
+import Testing
+@testable import SpotifyClone
+
+struct SpotifyCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
